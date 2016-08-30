518b1a5b4f901af3fadbd3a9b83482f402a544f9
LanCui <lancui19831110@163.com>
2016-08-30 17:31:55 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,32 @@
+/**
+* @Author: lancui
+* @Date:   2016-08-22 16:08:00
+* @Email:  lancui@superjia.com
+* @Last modified by:   lancui
+* @Last modified time: 2016-08-30 16:08:61
+*/
+
+
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import App from './App.vue';
+//
+// Vue.use(Router)
+// Vue.config.devtools = true
+//
+// const router = new Router()
+//
+// router.map({
+//   '/': {
+//     component: function (resolve) {
+//       require(['./Home'], resolve)
+//     }
+//   }
+// })
+//
+// router.start(App, '#app')
+// /* eslint-disable no-new */
+// new Vue({
+//   el: 'body',
+//   components: { App },
+// });
